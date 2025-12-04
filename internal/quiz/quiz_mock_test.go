eb150f1285278b7d74c5d2f9cbb502447d5c6a5b
starquake <starquake@linuxeverywhere.org>
2025-12-04 10:11:13 +0100

    Revert using sentinel errors for quiz, deleted unnecessary  test.

--- internal/quiz/quiz_mock_test.go
+++ internal/quiz/quiz_mock_test.go
@@ -45,39 +45,4 @@ func TestSQLiteStore_ListQuizzes_MockTesting(t *testing.T) {
 			t.Errorf("there were unfulfilled expectations: %s", err)
 		}
 	})
-
-	t.Run("error retrieving questions", func(t *testing.T) {
-		t.Parallel()
-
-		buf := bytes.Buffer{}
-		logger := logging.NewLogger(&buf)
-
-		db, mock, err := sqlmock.New()
-		if err != nil {
-			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-		}
-
-		testError := errors.New("questions error")
-
-		// Set up a mock that will return an error when the GetQuestionsByQuizIDSQL query is executed.
-		quizRows := mock.NewRows([]string{"id", "title", "slug", "description", "created_at"}).
-			AddRow(1, "Test Quiz", "test-quiz", "Test Description", 1234).
-			AddRow(2, "Another Quiz", "another-quiz", "Another Description", 5678)
-
-		mock.ExpectQuery(quiz.ListQuizzesSQL).WillReturnRows(quizRows).RowsWillBeClosed()
-		mock.ExpectQuery(quiz.GetQuestionsByQuizIDSQL).WithArgs(1).WillReturnError(testError)
-
-		quizStore := quiz.NewSQLiteStore(db, logger)
-
-		_, err = quizStore.ListQuizzes(t.Context())
-		if err == nil {
-			t.Fatal("expected error, got nil")
-		}
-		if !errors.Is(err, testError) {
-			t.Fatalf("expected error to be %v, got %v", testError, err)
-		}
-		if err = mock.ExpectationsWereMet(); err != nil {
-			t.Errorf("there were unfulfilled expectations: %s", err)
-		}
-	})
 }
