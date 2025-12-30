a5d54f7028d407c25f4e1f41d72f901031d8fe14
starquake <starquake@linuxeverywhere.org>
2025-12-30 11:56:21 +0100

    Extend integration tests for questions.

--- cmd/server/server_integration_test.go
+++ cmd/server/server_integration_test.go
@@ -64,10 +64,10 @@ func TestServer_Integration(t *testing.T) {
 	quizSlug := "integration-test-quiz"
 	quizDesc := "A quiz created by integration test"
 
-	form := url.Values{}
-	form.Add("title", quizTitle)
-	form.Add("slug", quizSlug)
-	form.Add("description", quizDesc)
+	quizForm := url.Values{}
+	quizForm.Add("title", quizTitle)
+	quizForm.Add("slug", quizSlug)
+	quizForm.Add("description", quizDesc)
 
 	client := &http.Client{
 		CheckRedirect: func(_ *http.Request, _ []*http.Request) error {
@@ -80,7 +80,7 @@ func TestServer_Integration(t *testing.T) {
 		ctx,
 		http.MethodPost,
 		fmt.Sprintf("http://%s/admin/quizzes", serverAddr),
-		strings.NewReader(form.Encode()),
+		strings.NewReader(quizForm.Encode()),
 	)
 	if err != nil {
 		t.Fatalf("failed to create request: %v", err)
@@ -103,8 +103,8 @@ func TestServer_Integration(t *testing.T) {
 		t.Logf("Response body: %s", string(body))
 	}
 
-	location := resp.Header.Get("Location")
-	if got, want := location, "/admin/quizzes/"; !strings.HasPrefix(got, want) {
+	quizLocation := resp.Header.Get("Location")
+	if got, want := quizLocation, "/admin/quizzes/"; !strings.HasPrefix(got, want) {
 		t.Errorf("got Location header %q, want prefix %q", got, want)
 	}
 
@@ -113,7 +113,7 @@ func TestServer_Integration(t *testing.T) {
 		ctx,
 		http.MethodGet,
 		fmt.Sprintf("http://%s/admin/quizzes", serverAddr),
-		strings.NewReader(form.Encode()),
+		strings.NewReader(quizForm.Encode()),
 	)
 	if err != nil {
 		t.Fatalf("failed to create request: %v", err)
@@ -142,8 +142,8 @@ func TestServer_Integration(t *testing.T) {
 	req, err = http.NewRequestWithContext(
 		ctx,
 		http.MethodGet,
-		fmt.Sprintf("http://%s%s", serverAddr, location),
-		strings.NewReader(form.Encode()),
+		fmt.Sprintf("http://%s%s", serverAddr, quizLocation),
+		strings.NewReader(quizForm.Encode()),
 	)
 	if err != nil {
 		t.Fatalf("failed to create request: %v", err)
@@ -168,6 +168,102 @@ func TestServer_Integration(t *testing.T) {
 		t.Errorf("string(body) = %q, should contain %q", got, want)
 	}
 	if got, want := string(body), quizDesc; !strings.Contains(got, want) {
+		t.Errorf("string(body) = %q, should contain %q", got, want)
+	}
+
+	questionText := "What is the name of the famous plumber wearing red and blue?"
+	questionOption1 := "Sonic"
+	questionOption2 := "Mario"
+	questionOption3 := "Tolls"
+	questionOption4 := "Kirby"
+
+	questionForm := url.Values{}
+	questionForm.Add("text", questionText)
+	questionForm.Add("position", "10")
+	questionForm.Add("option[0].text", questionOption1)
+	questionForm.Add("option[1].text", questionOption2)
+	questionForm.Add("option[1].correct", "on")
+	questionForm.Add("option[2].text", questionOption3)
+	questionForm.Add("option[3].text", questionOption4)
+
+	// Add question to quiz
+	req, err = http.NewRequestWithContext(
+		ctx,
+		http.MethodPost,
+		fmt.Sprintf("http://%s%s/questions", serverAddr, quizLocation),
+		strings.NewReader(questionForm.Encode()),
+	)
+	if err != nil {
+		t.Fatalf("failed to create request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatalf("failed to post quiz: %v", err)
+	}
+	defer func() {
+		closeErr := resp.Body.Close()
+		if closeErr != nil {
+			t.Errorf("failed to close response body: %v", closeErr)
+		}
+	}()
+	if got, want := resp.StatusCode, http.StatusSeeOther; got != want {
+		t.Errorf("expected status %d, got %d", want, got)
+		body, _ := io.ReadAll(resp.Body)
+		t.Logf("Response body: %s", string(body))
+	}
+
+	questionLocation := resp.Header.Get("Location")
+	if got, want := questionLocation, quizLocation; !strings.HasPrefix(got, want) {
+		t.Errorf("got Location header %q, want prefix %q", got, want)
+	}
+
+	// Verify question details using quiz details
+	req, err = http.NewRequestWithContext(
+		ctx,
+		http.MethodGet,
+		fmt.Sprintf("http://%s%s", serverAddr, quizLocation),
+		strings.NewReader(quizForm.Encode()),
+	)
+	if err != nil {
+		t.Fatalf("failed to create request: %v", err)
+	}
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatalf("failed to get quiz details: %v", err)
+	}
+	defer func() {
+		closeErr := resp.Body.Close()
+		if closeErr != nil {
+			t.Errorf("failed to close response body: %v", closeErr)
+		}
+	}()
+
+	body, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("failed to read body: %v", err)
+	}
+
+	if got, want := string(body), quizTitle; !strings.Contains(got, want) {
+		t.Errorf("string(body) = %q, should contain %q", got, want)
+	}
+	if got, want := string(body), quizDesc; !strings.Contains(got, want) {
+		t.Errorf("string(body) = %q, should contain %q", got, want)
+	}
+	if got, want := string(body), questionText; !strings.Contains(got, want) {
+		t.Errorf("string(body) = %q, should contain %q", got, want)
+	}
+	if got, want := string(body), questionOption1; !strings.Contains(got, want) {
+		t.Errorf("string(body) = %q, should contain %q", got, want)
+	}
+	if got, want := string(body), questionOption2; !strings.Contains(got, want) {
+		t.Errorf("string(body) = %q, should contain %q", got, want)
+	}
+	if got, want := string(body), questionOption3; !strings.Contains(got, want) {
+		t.Errorf("string(body) = %q, should contain %q", got, want)
+	}
+	if got, want := string(body), questionOption4; !strings.Contains(got, want) {
 		t.Errorf("string(body) = %q, should contain %q", got, want)
 	}
 
